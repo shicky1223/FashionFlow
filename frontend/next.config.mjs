c24a854982227639b7c7cbf914ecc91f90bb0774
Khushi Anumalla <khushianumalla@gmail.com>
2025-04-05 17:50:13 -0400

    frontendeupdat

--- frontend/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-      DATABASE_URL: process.env.DATABASE_URL,
-      JWT_SECRET: process.env.JWT_SECRET,
-    },
-  }
-  
-  export default nextConfig
